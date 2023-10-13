dbd527a5375598fc89668d80fa62300c03090a3a
martypavalko <marty.pavalko@gmail.com>
2023-10-12 21:51:50 -0400

    added grub support

--- configuration.nix
+++ configuration.nix
@@ -12,8 +12,11 @@
     ];
 
   # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader = {
+    loader.systemd-boot.enable = true;
+    loader.efi.canTouchEfiVariables = true;
+    grub.enable = true;
+  }
 
   networking.hostName = "marty-nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
